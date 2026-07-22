698931130c2ecd83f131fafffd83442c08e72358
erfan <erfanbanaei110@gmail.com>
2026-07-22 23:06:35 +0330

    Add branding images for light and dark modes in Assets.xcassets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 
 import 'screens/home_screen.dart';
@@ -7,7 +8,8 @@ import 'theme/app_theme.dart';
 import 'theme/theme_controller.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   final themeController = ThemeController();
   await themeController.load();
   runApp(QrApp(themeController: themeController));
@@ -37,10 +39,52 @@ class QrApp extends StatelessWidget {
             theme: AppTheme.light,
             darkTheme: AppTheme.dark,
             themeMode: controller.themeMode,
-            home: const HomeScreen(),
+            home: const _SplashFadeIn(child: HomeScreen()),
           );
         },
       ),
+    );
+  }
+}
+
+/// اسپلش نیتیو را تا آماده شدن اولین فریم واقعی نگه می‌دارد، سپس آن را کنار
+/// می‌زند و محتوای اپ را با یک فیدِ نرم روی همان رنگ پس‌زمینه‌ی اسپلش نشان
+/// می‌دهد تا گذار ناگهانی و پرشی حس نشود.
+class _SplashFadeIn extends StatefulWidget {
+  final Widget child;
+
+  const _SplashFadeIn({required this.child});
+
+  @override
+  State<_SplashFadeIn> createState() => _SplashFadeInState();
+}
+
+class _SplashFadeInState extends State<_SplashFadeIn> with SingleTickerProviderStateMixin {
+  late final _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 320),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      FlutterNativeSplash.remove();
+      _controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+      color: const Color(0xFF0E0B1A),
+      child: FadeTransition(opacity: _controller, child: widget.child),
     );
   }
 }
--- tool/check_band.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-import 'package:image/image.dart' as img;
-void main() {
-  final im = img.decodePng(File('assets/splash_background.png').readAsBytesSync())!;
-  for (int y = 1600; y < 1700; y += 5) {
-    final p = im.getPixel(540, y);
-    print('y=$y -> ${p.r},${p.g},${p.b}');
-  }
-}
